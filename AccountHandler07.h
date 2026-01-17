5811a27c72655c14a4d51f6ff7ce055324fc7af7
BBongCoding <ihhan01@gmail.com>
2026-01-17 23:50:35 +0900

    oop.7

---
+++ AccountHandler07.h
@@ -0,0 +1,19 @@
+#pragma once
+#include"BankingCommonDecl07.h"
+#include"Account07.h"
+#include"AccountArray.h"
+
+class AccountHandler
+{
+private:
+    //Account* accArr[100]; // Account 저장을 위한 배열
+    BoundCheckAccountPtrArray accArr;
+    int accNum;          // 저장된 Account 수
+public:
+    AccountHandler() :accNum(0),accArr(100) {};
+    void ShowMenu(void);       // 메뉴출력
+    void MakeAccount(void);    // 계좌개설을 위한 함수
+    void DepositMoney(void);       // 입    금
+    void WithdrawMoney(void);      // 출    금
+    void ShowAllAccInfo(void);     // 잔액조회
+};
